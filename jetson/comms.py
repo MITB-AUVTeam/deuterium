edd6c3d96775a066f3b3d80fd738380fa87110f9
ChaturVasireddy <chaturvasireddy@gmail.com>
2026-04-18 19:55:13 +0530

    fix

--- jetson/comms.py
+++ jetson/comms.py
@@ -1,28 +1,45 @@
 import usb.core
 import usb.util
+import struct
+import time
 
+#find device
 dev = usb.core.find(idVendor=0x2e8a, idProduct=0x000a)
 
 if dev is None:
     raise ValueError("Device not found")
 
-# 🔥 DETACH kernel drivers FIRST
-for cfg in dev:
-    for intf in cfg:
-        if dev.is_kernel_driver_active(intf.bInterfaceNumber):
-            print("Detaching interface", intf.bInterfaceNumber)
-            dev.detach_kernel_driver(intf.bInterfaceNumber)
+# Detach ONLY CDC interfaces (0 and 1)
+for i in (0, 1):
+    try:
+        if dev.is_kernel_driver_active(i):
+            dev.detach_kernel_driver(i)
+    except:
+        pass  # ignore if not active
 
-# NOW safe
 dev.set_configuration()
 
-cfg = dev.get_active_configuration()
+#get vendor interface (2)
+intf = dev.get_active_configuration()[(2, 0)]
 
-# Use vendor interface (usually 1)
-intf = cfg[(1, 0)]
+usb.util.claim_interface(dev, 2)
 
-usb.util.claim_interface(dev, intf.bInterfaceNumber)
+#get OUT endpoint
+ep_out = usb.util.find_descriptor(
+    intf,
+    custom_match=lambda e:
+        usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_OUT
+)
 
-for cfg in dev:
-    for intf in cfg:
-        print("Interface:", intf.bInterfaceNumber)
+if ep_out is None:
+    raise ValueError("OUT endpoint not found")
+
+#send loop
+while True:
+    data = struct.pack('fff', 1.1, 2.2, 3.3)
+
+    ep_out.write(data)
+
+    print("Sent:", struct.unpack('fff', data))
+
+    time.sleep(1)
